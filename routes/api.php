d7830ecd7fc7e5a3aa562d2360e0a0470f50ef85
Napikk <nafip3886@gmail.com>
2025-04-30 10:14:32 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,33 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::apiResource('employee', EmployeeController::class);
+Route::apiResource('salaries', SalaryController::class);
+Route::apiResource('salary_claims', SalaryClaimController::class);
+Route::post('attendances', [AttendanceController::class , 'store']);
+Route::get('/salary_approval_logs', [SalaryApprovalLogController::class, 'index']);
+
+// hr
+Route::middleware(['auth:sanctum', 'role:hr'])->group(function () {
+    Route::post('/salaries/{id}/approve', [SalaryController::class, 'approve']);
+    Route::get('/salary-approval-logs', [SalaryApprovalLogController::class, 'index']);
+});
+
+// admin
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/attendances/{id}/approve', [AttendanceController::class, 'approve']);
+    Route::get('/attendance-approval-logs', [AttendanceApprovalLogController::class, 'index']);
+});
+
+// karyawan
+Route::middleware(['auth:sanctum', 'role:karyawan'])->group(function () {
+    Route::post('/salary-claims', [SalaryClaimController::class, 'store']);
+    Route::get('/salary-claims', [SalaryClaimController::class, 'index']);
+    
+    Route::post('/attendances', [AttendanceController::class, 'store']);
+    Route::get('/attendances/my', [AttendanceController::class, 'myAttendances']);
+});
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
