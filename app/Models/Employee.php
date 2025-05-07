63f2999e7c81690969d8910c3686f4dc72d76321
Napikk <nafip3886@gmail.com>
2025-05-07 10:45:58 +0700

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -11,8 +11,33 @@ class Employee extends Model
 
     protected $fillable = [
         'user_id',
-        'employee_code',
-        'position',
-        'hire_date',
+        'full_name',
+        'position_id',
+        'email',
+        'phone_number',
+        'address',
+        'image',
     ];
+
+    public function attendance(){
+        return $this->hasMany(Attendance::class);
+    }
+
+    public function attendanceapproval(){
+        return $this->hasMany(AttendanceApprovalLog::class);
+    }
+
+    public function position(){
+        return $this->belongsTo(Position::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function salary(){
+        return $this->belongsTo(Salary::class);
+    }
+
+
 }
