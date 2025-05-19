5528ce54080c31e2ddafced58ec468c8dbb16169
Napikk <nafip3886@gmail.com>
2025-05-19 14:29:14 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -33,21 +33,19 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/salaries', [SalaryController::class, 'index']);
         Route::get('/attendance-approval-logs', [AttendanceApprovalLogController::class, 'index']);
         Route::get('/salary-approval-logs', [SalaryApprovalLogController::class, 'index']);
-        
+        Route::get('/absensi/all', [AbsensiController::class, 'index']);
     });
 
     //kabeh iki
     Route::middleware('role:HR,admin,karyawan')->group(function () {
         Route::get('/employees', [EmployeeController::class, 'index']);
+        Route::get('/absensi/histori', [AbsensiController::class, 'histori']);
     });
 
     //hr
     Route::middleware('role:HR')->group(function () {
-        Route::post('/salaries/{id}/approve', [SalaryController::class, 'approve']);
-        // Route::get('/salary-approval-logs', [SalaryApprovalLogController::class, 'index']);
-
-        Route::post('/attendances/{id}/gg', [AttendanceController::class, 'approve']);
-        // Route::get('/attendance-approval-logs', [AttendanceApprovalLogController::class, 'index']);
+        Route::post('/attendances/{id}/approve', [AttendanceController::class, 'approve']);
+        Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']);
 
         Route::get('/users/hr', [UserController::class, 'index']);
         // Route::get('/users', [UserController::class, 'index']);
@@ -67,17 +65,11 @@ Route::middleware('auth:sanctum')->group(function () {
     
     //karyawan
     Route::middleware('role:karyawan')->group(function () {
-        Route::post('/salary-claims', [SalaryClaimController::class, 'store']);
-        Route::get('/salary-claims', [SalaryClaimController::class, 'index']);
-
-        Route::post('/salary/my', [SalaryController::class, 'store']);
 
         Route::post('/izin', [AttendanceController::class, 'store']);
-        // Route::get('/izin', [AttendanceController::class, 'index']);
 
         Route::post('/absen/masuk', [AbsensiController::class, 'absenMasuk']);
         Route::post('/absen/pulang', [AbsensiController::class, 'absenPulang']);
-        Route::get('/absensi/histori', [AbsensiController::class, 'histori']);
     });
 });
 
@@ -85,49 +77,3 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-//admin
-// Route::middleware('role:admin')->group(function () {
-//     // Route::get('/users', [UserController::class, 'index']);
-//     // Route::get('/position', [PositionController::class, 'index']);
-//     // Route::get('/attendance-approval-logs', [AttendanceApprovalLogController::class, 'index']);
-//     Route::get('/salary-approval-logs', [SalaryApprovalLogController::class, 'index']);
-//     Route::get('/employees', [EmployeeController::class, 'index']);
-// });
-// hr
-// Route::middleware(['auth:sanctum', 'role:HR'])->group(function () {
-//     Route::post('/salaries/{id}/approve', [SalaryController::class, 'approve']);
-//     Route::get('/salary-approval-logs', [SalaryApprovalLogController::class, 'index']);
-
-//     Route::post('/attendances/{id}/approve', [AttendanceController::class, 'approve']);
-//     Route::get('/attendance-approval-logs', [AttendanceApprovalLogController::class, 'index']);
-
-//     Route::get('/users/hr', [UserController::class, 'index']);
-//     Route::get('/users', [UserController::class, 'index']);
-//     Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-// Route::get('/position', [PositionController::class, 'index']);
-// Route::post('/position', [PositionController::class, 'store']);
-// Route::put('/position/{id}', [PositionController::class, 'update']);
-// Route::delete('/position/{id}', [PositionController::class, 'destroy']);
-// Route::apiResource('positions', PositionController::class)->except('create');
-
-// Route::get('/employee', [EmployeeController::class, 'index']);
-// Route::post('/employee', [EmployeeController::class, 'store']);
-// Route::put('/employee/{id}', [EmployeeController::class, 'update']);
-// Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
-
-// });
-
-// admin
-// Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-
-
-
-// });
-
-// karyawan
-// Route::middleware(['auth:sanctum', 'role:karyawan'])->group(function () {
-
-// });
-
