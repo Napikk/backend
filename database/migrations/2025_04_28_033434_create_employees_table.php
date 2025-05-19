fe5d5666a08ffb5cc7906f2c87e7a3432cbbab5c
Napikk <nafip3886@gmail.com>
2025-05-19 14:29:23 +0700

    Update 2025_04_28_033434_create_employees_table.php

--- database/migrations/2025_04_28_033434_create_employees_table.php
+++ database/migrations/2025_04_28_033434_create_employees_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone_number',);
             $table->string('address');
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
